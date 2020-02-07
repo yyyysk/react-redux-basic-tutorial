6443434e97c09420eac85f0a04a84f1bc525a00e
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-02-07 13:44:24 +0900

    UI

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -4,13 +4,13 @@ import { VISIBILITY_FILTERS } from '../constants';
 export const getTodosState = store => store.todos;
 
 // 全てのTodoのIdの配列を取得
-export const getTodoList = store => getTodosState(store) ? ...getTodosState(store).allIds : [];
+export const getTodoList = store => getTodosState(store) ? getTodosState(store).allIds : [];
 
 // IDで指定してTODOの情報を取得
 export const getTodoById = (store, id) => getTodosState(store) ? {...getTodosState(store).byIds[id], id} : {};
 
 // 全てのTODOの情報を取得
-export const getTodods = store => getTodoList(store).map(id => getTodoById(store, id));
+export const getTodos = store => getTodoList(store).map(id => getTodoById(store, id));
 
 // フィルター条件
 export const getTodosByVisibilityFilter = (store, visibilityFilter) => {
